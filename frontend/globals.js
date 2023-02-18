ff2d4dc6b8c8b6ccbf16295f127c0f102cdc2541
sserve-kr <personal@sserve.me>
2023-02-18 16:22:16 +0900

    Changed backend location for production

--- frontend/globals.js
+++ frontend/globals.js
@@ -1,7 +1,7 @@
 let dev = false;
 
 const loc = {
-  backend: (path) => dev ? `http://localhost:8000${path}` : `http://backend:8000${path}`,
+  backend: (path) => dev ? `http://localhost:8000${path}` : `http://blogbackend:8000${path}`,
   cdn: (path) => `https://cdn.sserve.work/${path}`
 }
 
