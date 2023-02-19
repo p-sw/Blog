eae3fd8b00b277627dea3a48cb7af5c8fa00eddf
sserve-kr <personal@sserve.me>
2023-02-19 15:34:13 +0900

    Changed default theme to system

--- frontend/theme.js
+++ frontend/theme.js
@@ -3,7 +3,7 @@ import {mode} from "@chakra-ui/theme-tools";
 
 const theme = extendTheme({
     config: {
-        initialColorMode: "light", // IMPORTANT: change initialColorMode to "system" when prod mode
+        initialColorMode: "system",
         useSystemColorMode: false
     },
     fonts: {
